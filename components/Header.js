8d76e0793bb5f4988cf2aa8b8e78af724b74442e
Ayush <ayushj1405@gmail.com>
2022-11-12 16:15:36 +0530

    commit

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,10 @@
 import Image from "next/image";
-import React from "react";
 import Link from "next/link";
+import React from "react";
 import openseaLogo from "../assets/opensea.png";
+import { AiOutlineSearch } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
+import { MdOutlineAccountBalanceWallet } from "react-icons/md";
 
 const style = {
   wrapper: `bg-[#04111d] w-screen px-[1.2rem] py-[0.8rem] flex `,
@@ -14,15 +17,39 @@ const style = {
   headerItem: `text-white px-4 font-bold text-[#c8cacd] hover:text-white cursor-pointer`,
   headerIcon: `text-[#8a939b] text-3xl font-black px-4 hover:text-white cursor-pointer`,
 };
+
 const Header = () => {
   return (
-    <div className="{style.wrapper}">
-      <Link href="/profile">
-        <div className="{style.logoContainer">
+    <div className={style.wrapper}>
+      <Link href="/">
+        <div className={style.logoContainer}>
           <Image src={openseaLogo} height={40} width={40} />
           <div className={style.logoText}>Opensea</div>
         </div>
       </Link>
+      <div className={style.searchBar}>
+        <div className={style.searchIcon}>
+          <AiOutlineSearch />
+        </div>
+        <input
+          className={style.searchInput}
+          placeholder="Search items, collections, and accounts"
+        />
+      </div>
+      <div className={style.headerItems}>
+        <Link href="/collections/0x8303dD57BA32d4fD5fdABC18658Ce64A35E14F70">
+          <div className={style.headerItem}> Collections </div>
+        </Link>
+        <div className={style.headerItem}> Stats </div>
+        <div className={style.headerItem}> Resources </div>
+        <div className={style.headerItem}> Create </div>
+        <div className={style.headerIcon}>
+          <CgProfile />
+        </div>
+        <div className={style.headerIcon}>
+          <MdOutlineAccountBalanceWallet />
+        </div>
+      </div>
     </div>
   );
 };
